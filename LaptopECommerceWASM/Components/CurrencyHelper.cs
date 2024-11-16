debb33c86da0162eacc76c91898166be4248b2c4
Anh Dat <anh23dat.truong@gmail.com>
2024-11-16 14:34:39 +0700

    config home page

---
+++ LaptopECommerceWASM/Components/CurrencyHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+
+namespace LaptopECommerceWASM.Components
+{
+    public static class CurrencyHelper
+    {
+        private static readonly string _currencyFormat;
+
+        static CurrencyHelper()
+        {
+            // Đọc định dạng từ appsettings.json
+            var configuration = new ConfigurationBuilder()
+                .AddJsonFile("appsettings.json")
+                .Build();
+            _currencyFormat = configuration["CurrencyFormat"] ?? "#,##0₫";
+        }
+
+        public static string FormatCurrency(int price)
+        {
+            return price.ToString(_currencyFormat, CultureInfo.InvariantCulture);
+        }
+    }
+}
